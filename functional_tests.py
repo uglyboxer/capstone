281fb9b653ec662cdb502adbdf90dda93fb57f36
Cole Howard <uglyboxer@gmail.com>
2015-11-17 14:19:02 -0800

    First tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_page_view(self):
+        # User navigates to site
+        self.browser.get('http://localhost:8000')
+
+        # User sees "Finnegan" in title bar
+        assert 'Finnegan' in self.browser.title
+
+        # User sees "Finnegan" in header text
+
+        # A window on the right side of the screen shows a handwritten
+        # number.
+
+        # A button to submit 'nothing' is presented on the left side
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
