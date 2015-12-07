327134603ceb71617a12eb7c24bfd44919c10636
Cole Howard <uglyboxer@gmail.com>
2015-12-07 13:55:52 -0800

    Config file

--- net/settings.py
+++ net/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'netdb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
