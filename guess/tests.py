ea67606b0a6497d049f9815c81d25ec8a4352048
Cole Howard <uglyboxer@gmail.com>
2015-12-30 13:20:57 -0800

    Test update

--- guess/tests.py
+++ guess/tests.py
@@ -12,14 +12,7 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
-
     def test_post_request_resolves_to_results(self):
         c = Client()
-        response = c.post('/data/', {'info': '2'})
-        self.assertEqual(response.status_code, 302)
+        response = c.post('/sketch/', {'info': '2'})
+        self.assertEqual(response.status_code, 200)
